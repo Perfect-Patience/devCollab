7630fff79210fd965811a641c5c64dcc10cb1fa5
Perfect <patiencedah@gmail.com>
2025-09-07 10:09:45 +0000

    change redux folder structure

--- src/redux-app/store.js
+++ src/redux-app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from '../features/user/userSlice';
-import projectReducer from '../features/project/projectSlice';
+import userReducer from './features/user/userSlice';
+import projectReducer from './features/project/projectSlice';
 
 export const store = configureStore({
     reducer: {
